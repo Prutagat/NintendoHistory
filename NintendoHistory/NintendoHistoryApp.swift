ca23dc96e516f022edd5b4bd05aa3a95bc79d897
Prutagat <a.v.golovanov@icloud.com>
2024-04-01 21:13:53 +0300

    Initial Commit

---
+++ NintendoHistory/NintendoHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NintendoHistoryApp.swift
+//  NintendoHistory
+//
+//  Created by Алексей Голованов on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct NintendoHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
